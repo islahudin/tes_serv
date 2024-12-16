86960f2417d6159cfbf6ed907df0ff64261be691
islahdev <islahdev@gmail.com>
2024-12-16 12:11:56 +0700

    add deploy update l6

--- deploy.php
+++ deploy.php
@@ -35,13 +35,15 @@
 
 
 
-$secret = 'mauikanBakarGa1';
-$signature = 'sha1=' . hash_hmac('sha1', file_get_contents('php://input'), $secret);
-if ($_SERVER['HTTP_X_HUB_SIGNATURE'] !== $signature) {
-    http_response_code(403);
-    die('Unauthorized');
-}
+// $secret = 'mauikanBakarGa1';
+// $signature = 'sha1=' . hash_hmac('sha1', file_get_contents('php://input'), $secret);
+// if ($_SERVER['HTTP_X_HUB_SIGNATURE'] !== $signature) {
+//     http_response_code(403);
+//     die('Unauthorized');
+// }
 
 $output = shell_exec('cd /www/wwwroot/merantiapi.qordinate.com/tes_serv && git pull origin main 2>&1');
 echo "<pre>$output</pre>";
+
+
 
