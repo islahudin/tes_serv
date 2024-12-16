825faf3246a806a1ba31427e518dc692abfcef8f
islahdev <islahdev@gmail.com>
2024-12-16 11:00:25 +0700

    create webhook deploy update 2

--- deploy.php
+++ deploy.php
@@ -6,5 +6,5 @@
 //     http_response_code(403);
 //     die('Unauthorized');
 // }
-$output = shell_exec('cd public_html/goapi.qordinate.com/tes_serv && git pull origin main 2>&1');
+$output = shell_exec('cd /public_html/goapi.qordinate.com/tes_serv && git pull origin main 2>&1');
 echo "<pre>$output</pre>";
